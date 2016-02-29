96d223a4878b62db4c5d32bb98cce11c8ffa9968
kiarash14 <omidbakhsh22@gmail.com>
2016-02-29 21:55:04 +0330

    Update spanish_lang.lua

--- plugins/spanish_lang.lua
+++ plugins/spanish_lang.lua
@@ -348,8 +348,8 @@ end
 
 return {
 	patterns = {
-		'#(install) (spanish_lang)$',
-		'#(update) (spanish_lang)$'
+		'[!/#](install) (spanish_lang)$',
+		'[!/#](update) (spanish_lang)$'
 	},
 	run = run,
 }
